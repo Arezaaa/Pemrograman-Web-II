f1bb37ffb4bf739c54f2f66700769a75d474f005
Arezaaa <115351385+Arezaaa@users.noreply.github.com>
2024-04-01 09:35:52 +0800

    Add files via upload

---
+++ Praktikum 1/PRAK101.php
@@ -0,0 +1,5 @@
+<?php 
+echo "Hello World in PHP Nama<br>";
+echo "Nama Praktikan : {Ahmad Reza Alfayiet}<br>";
+echo "NIM : {2210817210016}";
+?>
---
+++ Praktikum 1/PRAK102.php
@@ -0,0 +1,12 @@
+<?php
+ $jarijari = 4.2;
+ $tinggi = 5.4;
+ $panjang = 8.9;
+ $lebar = 14.7;
+ $sisi = 7.9;
+
+$luas = ($tinggi/2)*$sisi;
+$volume = $luas*$tinggi;
+
+echo "$volume m3"
+?>
